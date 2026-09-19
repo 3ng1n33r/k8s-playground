c9ffe829aaf2b0fac89a491ee923db7c28f2835a
3ng1n33r <3ng1n33r@MacBook-Air-2.lan>
2026-09-19 17:44:19 +0500

    [develop-190926] move to roles in ansbile, refactoring

--- ansible/playbooks/files/cfssl/kube-etcd-healthcheck-client.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cfssl gencert -ca=etcd-ca.crt -ca-key=etcd-ca.key -config=ca-config.json -profile=client etcd-healthcheck-client-csr.json | cfssljson -bare etcd-healthcheck-client
-
-mv etcd-healthcheck-client.pem healthcheck-client.crt
-mv etcd-healthcheck-client-key.pem healthcheck-client.key
-rm -f -- *csr
-
-#kubectl -n monitoring create secret generic etcd-client-cert --from-file=etcd-ca.crt --from-file=healthcheck-client.crt --from-file=healthcheck-client.key
